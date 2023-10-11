db79ea838b2002d690511aefa48f79f583bae9e7
Adetona Mustapha <adetona3005@gmail.com>
2023-10-11 22:05:09 +0100

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-
 """
-    Method that determines the number of minmum operations given n characters
+Minimum Operations
 """
 
 
 def minOperations(n):
     """
-        A function that calculates the fewest number of operations
-        needed to give a result of exactly n H characters in a file
-        args: n: Number of characters to be displayed
-        return:
-               number of min operations
+    calculate the minimum operations
     """
+    number = n
+    div = 2
+    cnt = 0
 
-    now = 1
-    start = 0
-    counter = 0
-    while now < n:
-        remainder = n - now
-        if (remainder % now == 0):
-            start = now
-            now += start
-            counter += 2
+    while (number > 1):
+        if number % div == 0:
+            number = int(number / div)
+            cnt += div
+            div = 2
         else:
-            now += start
-            counter += 1
-    return counter
+            div += 1
+
+    return cnt
